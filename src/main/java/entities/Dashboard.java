595521985fdfb7986fca82499758a544ab88dc84
Yakim Rachev <jakimcho@gmail.com>
2019-07-15 11:27:05 +0300

    Added some helpers for entities creation
    (cherry picked from commit f8f50e31fddf29444dd041142549295222c99f52)

---
+++ src/main/java/entities/Dashboard.java
@@ -0,0 +1,75 @@
+package entities;
+
+import utils.AbstractEntitiesBuilder;
+
+public class Dashboard
+{
+    private String name;
+    private String description;
+
+    public Dashboard( String name,
+                      String description )
+    {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName( )
+    {
+        return name;
+    }
+
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+
+    public String getDescription( )
+    {
+        return description;
+    }
+
+    public void setDescription( String description )
+    {
+        this.description = description;
+    }
+
+    @Override
+    public String toString( )
+    {
+        StringBuilder sb = new StringBuilder( );
+        sb.append( " name: " )
+          .append( this.name )
+          .append( "description: " )
+          .append( this.description );
+
+        return sb.toString( );
+    }
+
+    ////////////////////////////// Builder ////////////////////////
+    public static class Builder
+            extends AbstractEntitiesBuilder<Dashboard.Builder>
+    {
+        private String name;
+        private String description;
+
+        public Dashboard build( )
+        {
+            return new Dashboard( this.name,
+                                  this.description );
+        }
+
+        public Builder setDescription( String description )
+        {
+            this.description = description;
+            return this;
+        }
+
+        public Builder setName( String name )
+        {
+            this.name = name;
+            return this;
+        }
+    }
+
+}
